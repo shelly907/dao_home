adc9db27ec8bdfb8216ee88c22764a3ada2508aa
shelly907 <shellyparekh9@gmail.com>
2024-06-26 10:46:05 -0700

    Initial Commit

---
+++ dao_home/dao_homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  dao_homeApp.swift
+//  dao_home
+//
+//  Created by Scholar on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct dao_homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
